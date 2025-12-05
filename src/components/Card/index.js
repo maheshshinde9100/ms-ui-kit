af1efe902c1ee22e184ce06cb95747ad8205129c
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-12-05 21:15:09 +0530

    change directory of card ui components

---
+++ src/components/Card/index.js
@@ -0,0 +1,8 @@
+import Card from './Card';
+import CardHeader from './CardHeader';
+import CardBody from './CardBody';
+import CardFooter from './CardFooter';
+import CardMedia from './CardMedia';
+
+export default Card;
+export { Card, CardHeader, CardBody, CardFooter, CardMedia };
